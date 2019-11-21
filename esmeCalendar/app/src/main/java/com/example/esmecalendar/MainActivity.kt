c01157048fe8c504a55540c540f99ee10b0d3079
Pala <j-palladin@sfr.fr>
2019-11-21 17:15:33 +0100

    bbb

--- esmeCalendar/app/src/main/java/com/example/esmecalendar/MainActivity.kt
+++ esmeCalendar/app/src/main/java/com/example/esmecalendar/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.esmecalendar
 
 import android.content.Context
+import android.content.Intent
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.example.esmecalendar.adapters.EventAdapter
 import com.example.esmecalendar.interfaces.IEventInterface
@@ -12,9 +16,8 @@ import com.example.esmecalendar.mocks.EventsMock
 import com.example.esmecalendar.models.Event
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-
 class MainActivity : AppCompatActivity(), ItemClickListener {
+
 
     var eventsRepository: IEventInterface = EventsMock()
 
@@ -22,9 +25,18 @@ class MainActivity : AppCompatActivity(), ItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         rv_event_list.layoutManager = LinearLayoutManager(this)
-
         getEventsInThread()
+        val btnOpenActivity : Button = findViewById(R.id.button)
+        btnOpenActivity.setOnClickListener {
+            val intent = Intent(this,Main3Activity :: class.java)
+            startActivity(intent)
+        }
 
+        val btnOpenActivity2 : Button = findViewById(R.id.button2)
+        btnOpenActivity2.setOnClickListener {
+            val intent = Intent(this,Main2Activity :: class.java)
+            startActivity(intent)
+        }
     }
 
     fun getEventsInThread() {
@@ -63,11 +75,26 @@ class MainActivity : AppCompatActivity(), ItemClickListener {
 
 
     override fun onClickItem(event :Event){
+
+        val intent = Intent(applicationContext, ChatActivity::class.java)
+
+        intent.putExtra("EVENT", event.id)
+
+        startActivity(intent)
+
         Toast.makeText(applicationContext,"Interface marche",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onClickClassRoom(classroom: String) {
+        val intent = Intent(applicationContext, LocationActivity::class.java)
+
+        startActivity(intent)
+        Toast.makeText(applicationContext,"Class"+classroom,Toast.LENGTH_SHORT).show()
     }
 
 }
 
 interface ItemClickListener{
     fun onClickItem(event :Event)
+    fun onClickClassRoom(classroom : String)
 }
